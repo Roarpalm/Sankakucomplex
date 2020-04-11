cda58dd4489a59f1a1a6ee105644a5a3c14e6d63
Roarpalm <307687673@qq.com>
2020-04-11 10:22:38 +0800

    Update3.1

--- sankaku.py
+++ sankaku.py
@@ -1,12 +1,14 @@
 import tkinter as tk
+import tkinter.messagebox
 import asyncio, aiohttp, aiofiles, os
 from time import time
 from lxml import etree
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-import tkinter.messagebox
+
 
 class first():
+    '''爬排行榜，获取id'''
     def __init__(self):
         self.ids = []
         self.good_ids = []
@@ -14,12 +16,6 @@ class first():
         self.start_date = e1.get()
         self.end_date = e2.get()
         self.url = f'https://idol.sankakucomplex.com/?tags=date%3A{self.start_date}..{self.end_date}%20order%3Aquality'
-        self.header = {
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Host': 'idol.sankakucomplex.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-        }
 
         start = time()
         try:
@@ -34,21 +30,18 @@ class first():
     async def main(self):
         async with aiohttp.connector.TCPConnector(limit=300, force_close=True, enable_cleanup_closed=True, verify_ssl=False) as tc:
             async with aiohttp.ClientSession(connector=tc) as session:
-                # 创建文件
                 self.create_txt()
-                # 获取id
                 await self.get_id(session, self.url, self.n)
-                # 保存id
                 self.save_id()
             
     async def get_id(self, session, url, n):
         '''爬取图片id'''
         try:
-            response = await session.get(url, headers=self.header)
+            response = await session.get(url, headers=header)
         except:
             print('connect error...try again...')
             await asyncio.sleep(10)
-            response = await session.get(url, headers=self.header)
+            response = await session.get(url, headers=header)
         if self.n == 0:
             if response.status == 200:
                 print('HTTP:200 连接成功')
@@ -65,18 +58,22 @@ class first():
 
         # 获取下一页的Query String Parameters
         tree = etree.HTML(html)
-        if self.n == 0:
-            next_page = tree.xpath('//*[@id="post-list"]/div[3]/div[1]/@next-page-url')[0]
-        else:
-            next_page = tree.xpath('/html/body/div/@next-page-url')[0]
+        try:
+            if self.n == 0:
+                next_page = tree.xpath('//*[@id="post-list"]/div[3]/div[1]/@next-page-url')[0]
+            else:
+                next_page = tree.xpath('/html/body/div/@next-page-url')[0]
+        except AttributeError:
+            return
         self.n += 1
         next_url = 'https://idol.sankakucomplex.com/post/index.content' + next_page
         await asyncio.sleep(3)
         # n为下拉次数，既一个月份的页数
-        if self.n < 20:
+        if self.n < 40:
             await self.get_id(session, next_url, self.n)
 
     def create_txt(self):
+        '''首次运行创建文本文件'''
         id_ = 'id.txt'
         all_id_ = 'all id.txt'
         href_ = 'href.txt'
@@ -111,32 +108,38 @@ class first():
                 print(f'新增{len(self.good_ids)}个url到文本')
             # 备份
             with open('all id.txt', 'a') as f:
-                f.write(f'{self.start_date}..{self.end_date}:')
+                f.write(f'{self.start_date}..{self.end_date}:\n')
                 for i in self.good_ids:
                     f.write(i + '\n')
 
 
 
 class second():
+    '''通过id获取url'''
     def __init__(self):
         self.bad_ids = []
-        self.header = {
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Host': 'idol.sankakucomplex.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-        }
         with open('all href.txt', 'r') as f:
             self.old_hrefs = f.read().splitlines()
             print(f'已爬取{len(self.old_hrefs)}次')
-        
+        self.run_main()
+
+    def run_main(self):
         start = time()
         try:
             loop = asyncio.get_event_loop()
             future = asyncio.ensure_future(self.main())
             loop.run_until_complete(future)
         except (aiohttp.client_exceptions.ClientConnectionError, asyncio.exceptions.TimeoutError):
-            tkinter.messagebox.showerror(title='错误', message='网络连接中断，请再次尝试第二步')
+            with open('id.txt', 'r') as f:
+                all_id = f.read().splitlines()
+                true_id = []
+                for i in all_id:
+                    if i:
+                        true_id.append(i)
+            with open('href.txt', 'w') as f:
+                for i in true_id:
+                    f.write(i + '\n')
+            self.run_main()
         print(f'用时{int((time()-start) // 60)}分{int((time()-start) % 60)}秒')
         tkinter.messagebox.showinfo(title='Hi!', message='第二步已完成')
 
@@ -166,18 +169,20 @@ class second():
     async def get_href(self, session, sem, img_id, fail=False):
         '''获取url'''
         async with sem:
-            url = 'https://idol.sankakucomplex.com/post/show/' + str(img_id)
+            url = 'https://idol.sankakucomplex.com/post/show/' + img_id
             try:
-                response = await session.get(url, headers=self.header)
+                response = await session.get(url, headers=header)
             except:
                 # 请求失败等待10秒再次请求
                 print('connect error...try again...')
                 await asyncio.sleep(10)
-                response = await session.get(url, headers=self.header)
+                response = await session.get(url, headers=header)
             if response.status != 200:
-                # 请求过于频繁，等待120秒
+                # 请求过于频繁，等待150秒
                 print('Error 429 too many requests - please slow down...')
-                await asyncio.sleep(120)
+                if not fail:
+                    self.bad_ids.append(img_id)
+                await asyncio.sleep(150)
                 return
             html = await response.text()
             tree = etree.HTML(html)
@@ -212,14 +217,10 @@ class second():
 
 
 class third():
+    '''通过url下载文件'''
     def __init__(self):
         self.fail_url_list = []
-        self.name = f'{e1.get()}..{e2.get}'
-        self.header = {
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Host': 'idol.sankakucomplex.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+        self.name = f'{e1.get()}..{e2.get()}'
         self.download_header = {
             'Sec-Fetch-Dest': 'document',
             'Upgrade-Insecure-Requests': '1',
@@ -233,7 +234,16 @@ class third():
             future = asyncio.ensure_future(self.main())
             loop.run_until_complete(future)
         except (aiohttp.client_exceptions.ClientConnectionError, asyncio.exceptions.TimeoutError):
-            #tkinter.messagebox.showerror(title='错误', message='网络连接中断，请再次尝试第三步')
+            # 清除空行
+            with open('href.txt', 'r') as f:
+                all_href = f.read().splitlines()
+                true_href = []
+                for i in all_href:
+                    if i:
+                        true_href.append(i)
+            with open('href.txt', 'w') as f:
+                for i in true_href:
+                    f.write(i + '\n')
             self.run_main()
         print(f'用时{int((time()-start) // 60)}分{int((time()-start) % 60)}秒')
         tkinter.messagebox.showinfo(title='Hi!', message='第三步已完成')
@@ -291,6 +301,7 @@ class third():
             response = await session.get(href, headers=self.download_header)
             try:
                 file_size = int(response.headers['content-length'])
+                # 大于20M的文件不下载
                 if file_size > 20000000:
                     return
             except Exception as e:
@@ -335,36 +346,35 @@ class third():
                         await f.write(read_data.replace(href, ''))
 
                 except Exception as e:
-                    if fail:
-                        print(f'重新下载失败，原因：{e}')
-                    else:
-                        print(f'下载失败，原因：{e}')
+                    if not fail:
                         # 保存下载失败的url在fail_url_list
                         self.fail_url_list.append(href)
 
-win=tk.Tk() #构造窗体
+header = {
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Host': 'idol.sankakucomplex.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+
+win=tk.Tk() # 构造窗体
 win.title('Sankaku') # 给窗口的可视化起名字
 win.geometry('400x200')  # 设定窗口的大小(长 * 宽)
-
-l = tk.Label(win, text='开始日期：',font=('pingfang', 12))
-l.place(x=80,y=30,anchor='s')
-
-n = tk.Label(win, text='结束日期：',font=('pingfang', 12))
-n.place(x=260,y=30,anchor='s')
-
+# 文字
+l1 = tk.Label(win, text='开始日期：',font=('pingfang', 12))
+l1.place(x=80,y=30,anchor='s')
+l2 = tk.Label(win, text='结束日期：',font=('pingfang', 12))
+l2.place(x=260,y=30,anchor='s')
+# 输入框
 e1 = tk.Entry(win, show=None)
 e1.place(x=110,y=60,anchor='s')
-
 e2 = tk.Entry(win, show=None)
 e2.place(x=290,y=60,anchor='s')
-
-d = tk.Button(win, text='第一步', font=('pingfang', 12), width=12, height=2, command=first)
-d.place(x=80,y=180,anchor='s')
-
-e = tk.Button(win, text='第二步', font=('pingfang', 12), width=12, height=2, command=second)
-e.place(x=200,y=180,anchor='s')
-
-f = tk.Button(win, text='第三步', font=('pingfang', 12), width=12, height=2, command=third)
-f.place(x=320,y=180,anchor='s')
-
-win.mainloop() #进入消息循环
+# 按钮
+b1 = tk.Button(win, text='第一步', font=('pingfang', 12), width=12, height=2, command=first)
+b1.place(x=80,y=180,anchor='s')
+b2 = tk.Button(win, text='第二步', font=('pingfang', 12), width=12, height=2, command=second)
+b2.place(x=200,y=180,anchor='s')
+b3 = tk.Button(win, text='第三步', font=('pingfang', 12), width=12, height=2, command=third)
+b3.place(x=320,y=180,anchor='s')
+# 进入消息循环
+win.mainloop()
